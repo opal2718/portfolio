9d01861db54a0f8e4ab1645392b4930715f9932a
opal2718 <littlesihoo@gmail.com>
2024-03-09 15:51:15 +0900

    설문 수정 전체

--- assets/js/app.js
+++ assets/js/app.js
@@ -139,7 +139,7 @@ let footer = $(`
       <!--SVG code for quote-left icon-->
       <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1.5em" height="1.5em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path d="M7 21a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2L7 13a4 4 0 1 1 0 8zm10 0a4 4 0 0 1-4-4c0-1.473 1.333-6.14 4-14h2l-2 10a4 4 0 1 1 0 8z" fill="#fff" fill-rule="evenodd"/></svg>
       </div>
-      <p class="tag" style="font-family: "HangulLight";">기호 1번에 투표하여 우리 학교를 조금 더 나은 곳으로 바꾸어 나가는 과정에 힘을 보태주세요. <br>아래 양식을 이용해 제게 바라는 점을 익명으로 보내주실 수 있습니다.</p>
+      <p class="tag" style="font-family: "HangulLight";">아래 양식을 이용해 제게 바라는 점을 익명으로 보내주실 수 있습니다.</p>
     </div>
 
     
@@ -160,7 +160,7 @@ let footer = $(`
               <h6 class="display">Get in Touch</h6>
               </div>
                 <form id="foorm" style="height:90%" name="form1" action="https://script.google.com/macros/s/AKfycbx0pSp3h-lN5UxbxNTnvxty_ZeQkYOqY7yQQke5snqOB-rLX6hTRUK3reT2CipnbMRz/exec" method="POST" accept-charset="UTF-8" >             
-                  <textarea style="font-family: "HangulLight";" class="formArea" onInput="this.parentNode.dataset.replicatedValue = this.value" id="data" name="data" placeholder="이곳에 입력 후 Send Button을 클릭하면 전송이 완료됩니다.\n(시각 효과 없을 수 있음)" required style="resize:none"></textarea>
+                  <textarea style="font-family: "HangulLight";" class="formArea" onInput="this.parentNode.dataset.replicatedValue = this.value" id="data" name="data" placeholder="이곳에 입력 후 Send Button을 클릭하시면 전송이 완료됩니다." required style="resize:none"></textarea>
               
                   <div id="main">
                     <button id="lnch" type="submit" value="Send">Send</button>
@@ -380,11 +380,11 @@ $(function submitAnimation() {
       setTimeout(function () {
         $("#lnch").addClass("launched").text("SENT");
         $("#lnch_btn").addClass("launched");
-      }, 1500);
+      }, 100);
       // Wait for 2.2 seconds so that the send button animation can be fully played before submitting the form
       setTimeout(() => {
         document.querySelector('form').submit();
-      }, 2200);
+      }, 100);
     }
   });
 });
